15625f0bc9fca0b9467a53d05f5a9625b22c02e8
RestauranteChina <90224565+RestauranteChina@users.noreply.github.com>
2021-09-09 11:17:25 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -5,6 +5,6 @@ $correo=$_POST['Correo'];
 $telefono=$_POST['Teléfono'];
 $mensaje=$_POST['Mensaje'];
 $contenido="Nombre:". $Nombre . "\nCorreo: " . $Correo . "\nTeléfono: " . $Teléfono . "\nMensaje: " . $Mensaje;
-mail($destino, "Contacto", $contenido);
+mail($destino, "contactanos", $contenido);
 header("location:gracias.html");
 ?>
